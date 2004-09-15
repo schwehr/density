bba4e8279659f73d52639c64992e14ab5cc3ee46
Kurt Schwehr <schwehr@gmail.com>
2004-09-15 22:35:48 +0000

    Just starting

---
+++ xyzdensity.C
@@ -0,0 +1,58 @@
+
+/// \brief Convert an xyz point set into a volume density
+///        Handles everything known about a triangle in a mesh
+
+
+/***************************************************************************
+ * INCLUDES
+ ***************************************************************************/
+
+#include <cassert>
+
+#include <cstdlib>
+#include <cstdio>
+
+// C++ includes
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+#include <string>	// Good STL data types.
+#include <vector>
+
+// Local includes
+#include "Triangle.H"
+#include "Vector.H"	// GetNormal()
+
+using namespace std;
+
+/***************************************************************************
+ * LOCAL MACROS and DEFINES
+ ***************************************************************************/
+
+#ifdef __GNUC__
+#define UNUSED __attribute((__unused__))
+#else
+/*! \def UNUSED
+  \brief GNU CC attribute to denote unused paramters in function calls.
+  The attribute remove compiler warning for unused arguments and variable.  Only works
+  for GNU compilers such as gcc and g++.
+*/
+#define UNUSED
+#endif
+
+#ifdef REGRESSION_TEST
+#define FAILED_HERE cout <<  __FILE__ << ":" << __LINE__ << " test failed" << endl
+#else
+/*! \def FAILED_HERE
+  \brief Used FAILED_HERE; to emit a string that looks like a compiler warning
+  Use this to allow emacs to jump to this source line with C-x `
+*/
+#define FAILED_HERE // Empty
+#endif
+
+/***************************************************************************
+ * GLOBALS
+ ***************************************************************************/
+
+static const UNUSED char* RCSid ="@(#) $Id$";
