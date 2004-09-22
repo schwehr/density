dff947a620ccdb97b5257486d539d1bd49ce66a6
Kurt Schwehr <schwehr@gmail.com>
2004-09-22 19:36:48 +0000

    nothing yet

---
+++ volinfo.C
@@ -0,0 +1,28 @@
+// $Revision$  $Author$  $Date$
+
+/// \brief Load a voxel volume and tell use about it.
+///        
+
+
+/***************************************************************************
+ * INCLUDES
+ ***************************************************************************/
+
+#include <cassert>
+
+#include <cstdlib>
+#include <cstdio>
+
+// C++ includes
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+#include <string>	// Good STL data types.
+#include <vector>
+
+// Local includes
+#include "Density.H"
+
+using namespace std;
+
