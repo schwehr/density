c62545f6bc8920880e7363786cdeb2a3775af663
Kurt Schwehr <schwehr@gmail.com>
2004-10-06 23:47:23 +0000

    Works with the new UI. Added BAD_PARAMETRIC and BAD_FORMAT Added GetFiles(), GetFormat(), GetParametricType(),  DoS_Bootstrap()

--- s_bootstrap.C
+++ s_bootstrap.C
@@ -76,8 +76,204 @@ LoadS(const string filename,vector <SVec> &s,vector<float> &sigmas) {
 //////////////////////////////////////////////////////////////////////
 #ifndef REGRESSION_TEST  // NOT testing
 
-enum BootTypeEnum { SITE_PARAMETRIC, SAMPLE_PARAMETRIC };
+enum BootTypeEnum { BAD_PARAMETRIC, SITE_PARAMETRIC, SAMPLE_PARAMETRIC };
+enum FormatEnum {BAD_FORMAT, XYZ_FORMAT,TPR_FORMAT,S_FORMAT};
+#include "s_bootstrap_cmd.h" // Command line args
+#include "Eigs.H" // Let's us convert to other coords
 
+bool GetFiles(char **in_arg,vector<string> &inFiles) {
+  assert(in_arg);
+  assert(*in_arg); // Must be at least 1 file in!
+  // can not have more than 64000 input files
+  for (size_t i=0;i<64000 && in_arg[i];i++) {
+    inFiles.push_back(string(in_arg[i]));
+  }
+  return (true);
+}	
+
+BootTypeEnum GetParametricType(const int site_given, const int sample_given) {
+  if (site_given && sample_given) {
+    cerr << "ERROR: can not specify both site and sample paramteric bootstrap" <<endl;
+    exit (EXIT_FAILURE);
+  } else if (site_given) return(SITE_PARAMETRIC);
+  return(SAMPLE_PARAMETRIC); // default
+}  
+
+
+FormatEnum GetFormat(const char *format_arg) {
+  assert(format_arg);
+  string format(format_arg);
+  if (0==string("s").compare(format)) return(S_FORMAT);
+  if (0==string("tpr").compare(format)) return(TPR_FORMAT);
+  if (0==string("xyz").compare(format)) return(XYZ_FORMAT);
+  cerr << "ERROR: format must be one of s, tpr, or xyz" << endl;
+  exit(EXIT_FAILURE);
+  //return(BAD_FORMAT);
+}
+
+
+/// \param oneFile true if out1, out2, and out3 are all the same file
+bool DoS_Bootstrap(const vector<string> &inFiles,
+		   ofstream &out1, ofstream &out2, ofstream &out3,
+		   const int numout_arg, const FormatEnum format, const BootTypeEnum type,
+		   const int draw)
+{
+  bool ok=true;
+  assert(1==numout_arg || 3==numout_arg);
+  assert(0<draw);
+
+  // Setup random number generator
+  gsl_rng * r;  /* global generator */
+  const gsl_rng_type *T;
+  gsl_rng_env_setup();
+  T = gsl_rng_default;
+  r = gsl_rng_alloc (T);
+  { unsigned long int s;  getDevRandom(s);  gsl_rng_set (r, s); } // Set the Seed
+
+  S_Engine sengine; // For converting to xyz or ptr
+
+  vector<SVec> s;
+  vector<float> sigmas;
+
+  vector<float> Vmin(3,0), Vint(3,0), Vmax(3,0); // for xyz or tpr
+
+  for(size_t i=0;i<inFiles.size();i++) {
+    cout << "reading file " << inFiles[i] << endl;
+    s.clear(); sigmas.clear();
+    if (!LoadS(inFiles[i],s,sigmas)) {
+      cerr << "ERROR - can't load datafile, skipping: " << inFiles[i] << endl;
+      ok=false; continue;
+    }
+
+    const float siteSigma = (SITE_PARAMETRIC==type)?SiteSigma(s):-666.;
+
+    SVec newSample(6,0.);
+    out1 << setiosflags(ios::fixed) << setprecision(10);
+    out2 << setiosflags(ios::fixed) << setprecision(10);
+    out3 << setiosflags(ios::fixed) << setprecision(10);
+
+    // Draw out and bootstrap 'draw' number of samples
+    for (size_t i=0;i<size_t(draw); i++) {
+      switch (type) {
+      case SITE_PARAMETRIC:   BootstrapParametricSite   (s,siteSigma,newSample, r); break;
+      case SAMPLE_PARAMETRIC: BootstrapParametricSample (s,sigmas   ,newSample, r); break;
+      default:	assert(false);
+      }
+      // Now what do we do with the new sample?
+      switch(format) {
+      case S_FORMAT:
+	for (size_t i=0;i<newSample.size();i++) out1 << newSample[i] << " ";
+	break;
+      //case PTR_FORMAT: break;  NOT SUPPORTED YET
+      case XYZ_FORMAT:
+	{
+	  sengine.setS(newSample);
+	  sengine.getXYZ(KMIN, Vmin);
+	  sengine.getXYZ(KINT, Vint);
+	  sengine.getXYZ(KMAX, Vmax);
+	  if (1==numout_arg) {
+	    for (size_t i=0;i<3;i++) out3 << Vmin[i] << " ";  // V3
+	    for (size_t i=0;i<3;i++) out2 << Vint[i] << " ";  // V2
+	    for (size_t i=0;i<3;i++) out1 << Vmax[i] << " ";  // V1
+	  } else {
+	    for (size_t i=0;i<3;i++){out1<<Vmin[i]<<" "; out2<<Vint[i]<<" "; out3<<Vmax[i]<<" ";}
+	  }
+	} // case XYZ
+	break;
+      default: assert(false && "Hell in a hand basket");
+      }
+
+      switch (numout_arg) {
+      case 1: out1 << endl; break;
+      case 3: out1 << endl; out2 << endl; out3 << endl; break;
+      default: assert(false && "What are we gonna do now, man?!?!");
+      }
+    } // for draws
+
+
+
+  } // for inFiles
+
+
+  return (ok);
+}
+
+//////////////////////////////////////////////////////////////////////
+// NEW UI -- USING gengetopt
+//////////////////////////////////////////////////////////////////////
+
+
+int main (const int argc, char *argv[]) {
+  bool ok=true;
+
+  gengetopt_args_info a;
+  if (0!=cmdline_parser(argc,argv,&a)) {
+    cerr << "FIX: should never get here" << endl;
+    cerr << "Early exit" << endl;
+    return (EXIT_FAILURE);
+  }
+
+  vector<string> inFiles;
+  if (!GetFiles(a.in_arg,inFiles)) {cerr << "Doh!  What happened?" << endl; return(EXIT_FAILURE);}
+
+  if (1!=a.numout_arg && 3!=a.numout_arg) {
+    cerr << "ERROR: numout must be either 1 or 3" << endl
+	 << "   found: " << a.numout_arg << endl;
+    return(EXIT_FAILURE);
+  }
+
+  cout << "format: " << a.format_arg << endl;
+  const FormatEnum format = GetFormat(a.format_arg);
+
+  // Can't have S_FORMAT and 3 outfiles.  Does not make sense!!!
+  if (3==a.numout_arg && S_FORMAT==format) {
+    cerr << "ERROR:  you can't have 3 output files with the S_FORMAT." << endl
+	 << "  That would be ludicrous" << endl;
+    return (EXIT_FAILURE);
+  }
+
+
+  const BootTypeEnum type = GetParametricType(a.site_given,a.sample_given);
+#ifndef NDEBUG
+  cerr << "Bootstrap type: " << (SITE_PARAMETRIC==type?"site":"sample") << "parametric"<<endl;
+#endif  
+  
+  if (1==a.numout_arg) {
+    // just one file
+    ofstream out(a.out_arg,ios::out);
+    if (out.is_open()) {
+      if (!DoS_Bootstrap(inFiles, out,out,out, a.numout_arg, format, type, a.draw_arg)) {
+	ok=false; cerr << "ERROR:  " << argv[0] << " failed in bootstrap routine." << endl;
+      }
+    } else {ok=false; cerr << "Failed to open output file" << endl;}
+  } else {
+    // 3 different files.
+    const string o1Name(string(a.out_arg)+string("1"));
+    const string o2Name(string(a.out_arg)+string("2"));
+    const string o3Name(string(a.out_arg)+string("3"));
+
+    ofstream o1(o1Name.c_str(),ios::out);
+    ofstream o2(o2Name.c_str(),ios::out);
+    ofstream o3(o3Name.c_str(),ios::out);
+
+    if (!o1.is_open() || !o2.is_open() || !o3.is_open() ) ok=false;
+    if (ok && !DoS_Bootstrap(inFiles, o1,o2,o3, a.numout_arg, format, type, a.draw_arg)) {
+      ok=false; cerr << "ERROR:  " << argv[0] << " failed in bootstrap routine." << endl;
+    }
+
+  }
+
+
+  //if (!DoS_Bootstrap(inFiles, string(a.out_arg), a.numout_arg, format, type)) {
+
+  return (ok?EXIT_SUCCESS:EXIT_FAILURE);
+}
+
+
+//////////////////////////////////////////////////////////////////////
+// OLD UI
+//////////////////////////////////////////////////////////////////////
+#if 0
 int main(int argc, char *argv[]) {
   if (4!=argc) {
     cerr << endl
@@ -148,6 +344,12 @@ int main(int argc, char *argv[]) {
 
   return (EXIT_SUCCESS);
 }
+
+#endif // 0 old gui
+//////////////////////////////////////////////////////////////////////
+// OLD UI
+//////////////////////////////////////////////////////////////////////
+
 
 
 #endif // !REGRESSION_TEST
