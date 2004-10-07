62ead216add829307d2a707f30502c32d5edbee1
Kurt Schwehr <schwehr@gmail.com>
2004-10-07 23:51:46 +0000

    doxygen \file

--- xyzdensity.C
+++ xyzdensity.C
@@ -17,7 +17,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 */
-
+/// \file 
 /// \brief Convert an xyz point set into a volume density
 ///        Handles everything known about a triangle in a mesh
 
