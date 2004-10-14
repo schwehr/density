3e901e83e1d6ea1dcf2b7cfdfc0031da347d27b6
Kurt Schwehr <schwehr@gmail.com>
2004-10-14 15:38:04 +0000

    Does not do anything yet

---
+++ volmakecmap.C
@@ -0,0 +1,103 @@
+// $Revision$  $Author$  $Date$
+/*
+    Copyright (C) 2004  Kurt Schwehr
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+/// \brief Help make color maps
+///
+/// Want to be able to read things similiar to GMT Color Pallet Files
+/// (cpt).  For example: /sw/share/gmt/cpt/GMT_cool.cpt.
+///
+/// You CPT files need to go from [0..1]
+
+/***************************************************************************
+ * INCLUDES
+ ***************************************************************************/
+
+// C includes
+#include <cassert>
+
+#include <cstdlib>
+#include <cstdio>
+
+// C++ includes
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+#include <string>	// Good STL data types.
+#include <vector>
+
+// Libraries
+#include <Inventor/SbColor.h> // for HSV to RGB conversion
+
+
+// Local includes
+#include "xyz_iv_cmd.h"
+
+using namespace std;
+
+/***************************************************************************
+ * MACROS, DEFINES, GLOBALS
+ ***************************************************************************/
+
+#include "debug.H" // provides FAILED_HERE, UNUSED, DebugPrintf
+int debug_level=0;
+
+/// Let the debugger find out which version is being used.
+static const UNUSED char* RCSid ="$Id$";
+
+/***************************************************************************
+ * Local routines
+ ***************************************************************************/
+
+/***************************************************************************
+ * MAIN
+ ***************************************************************************/
+
+int main (int argc, char *argv[]) {
+  bool ok=true;
+
+  gengetopt_args_info a;
+  if (0!=cmdline_parser(argc,argv,&a)) {
+    cerr << "FIX: should never get here" << endl;
+    cerr << "Early exit" << endl;
+    return (EXIT_FAILURE);
+  }
+
+#ifdef NDEBUG
+  if (a.verbosity_given) {
+    cerr << "Verbosity is totally ignored for optimized code.  Continuing in silent mode" << endl;
+  }
+#else // debugging
+  debug_level = a.verbosity_arg;
+  DebugPrintf(TRACE,("Debug level = %d\n",debug_level));
+#endif
+
+  //////////////////////////////////////////////////////////////////////
+  // ERROR CHECK ARGS
+  //////////////////////////////////////////////////////////////////////
+
+
+  //////////////////////////////////////////////////////////////////////
+  // GET TO WORK YOU SLACKER
+  //////////////////////////////////////////////////////////////////////
+
+  return (ok?EXIT_SUCCESS:EXIT_FAILURE);
+}
+
