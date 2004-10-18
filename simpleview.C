8b5a9153f534650bf2c9db4fa0fb686fc72e8a5c
Kurt Schwehr <schwehr@gmail.com>
2004-10-18 15:20:16 +0000

    Nasty mess

--- simpleview.C
+++ simpleview.C
@@ -38,23 +38,35 @@
 // C++ includes
 #include <iostream>
 #include <fstream>
+#include <sstream> // string stream
+
+// STL includes
 #include <vector>
 #include <string>
 
 //
 // Inventor/Coin
 //
+// SoQt
 #include <Inventor/Qt/SoQt.h>
 #include <Inventor/Qt/viewers/SoQtExaminerViewer.h>
 
+// Top level includes
 #include <Inventor/SoOffscreenRenderer.h>
+#include <Inventor/SoOutput.h>
 
+// Sensors
 #include <Inventor/sensors/SoTimerSensor.h>
 
+// Actions
+#include <Inventor/actions/SoWriteAction.h>
+
+// Events
 #include <Inventor/events/SoMouseButtonEvent.h>
 #include <Inventor/events/SoLocation2Event.h>
 #include <Inventor/events/SoKeyboardEvent.h>
 
+// Nodes
 #include <Inventor/nodes/SoEventCallback.h>
 
 #include <Inventor/nodes/SoNode.h>
@@ -65,7 +77,8 @@
 #include <Inventor/nodes/SoPointLight.h>
 #include <Inventor/nodes/SoSeparator.h>
 
-#include <Inventor/draggers/SoTranslate1Dragger.h>
+// Draggers
+//#include <Inventor/draggers/SoTranslate1Dragger.h>
 #include <Inventor/draggers/SoSpotLightDragger.h>
 
 // Voleon Includes
@@ -91,32 +104,34 @@ static const UNUSED char* RCSid ="$Id$";
  ***************************************************************************/
 
 class SceneInfo {
-private:
-  uint8_t magicNumber; ///< Help make sure we have a valid SceneInfo structure 
 public:
   SceneInfo();
   SoCamera *camera;
   //SoNode *root;
   SoSeparator *root;
 
-  vector<SoSpotLightDragger *> draggerVec; // All of the draggers that have been added.
-  //SoSeparator *draggerSep;
-  SoSwitch *draggerSwitch;
+  vector<SoSpotLightDragger *> draggerVec; ///< All of the draggers that have been added.
+  SoSwitch *draggerSwitch; ///< On/off of the separators.  May add code to show one at a time
 
   gengetopt_args_info *a; ///< a for args.  These are the command line arguments
 
-  bool animating;  // set to true to start animation;
-  bool render_frames_to_disk;  // true, we write out each animated frame
+  bool animating;  ///< set to true to start animation;
+  bool render_frames_to_disk;  ///< true, we write out each animated frame
 
-
+  /// Use magic numbers to make sure you actually get this class when you
+  /// must do a cast to/from void pointer
   static uint8_t nominalMagicNumber() {return(178);}
-  uint8_t getMagicNumber() const {return(magicNumber);}
+  //uint8_t getMagicNumber() const {return(magicNumber);}
+  bool magicOk() const {return(nominalMagicNumber()==magicNumber);} ///< Test the class integrity
+private:
+  uint8_t magicNumber; ///< Help make sure we have a valid SceneInfo structure 
 };
 
 /// Make a SceneInfo object with all pointers set to zero and not animating
 SceneInfo::SceneInfo() {
   magicNumber = nominalMagicNumber();
-  camera = 0; root = 0; draggerSwitch = 0; draggerSwitch = 0; a = 0;
+  camera = 0; root = 0; draggerSwitch = 0; draggerSwitch = 0;
+  a = 0;
   animating=render_frames_to_disk=false;
 }
 
@@ -210,7 +225,7 @@ InterpolatePos(const vector<float> &v1, const vector<float> &v2,
 /// \param root Top of the scene graph to render
 /// \param frame_num return the frame number that we wrote
 /// \return \a false if we flailed trying to get a pretty picture to disk
-bool RenderFrameToDisk (const string &basename_arg,const string &filetype_arg,
+bool RenderFrameToDisk (const string &basename,const string &filetype,
 			const int width, const int height,
 			SoNode *root, size_t &frame_num)
 {
@@ -230,23 +245,23 @@ bool RenderFrameToDisk (const string &basename_arg,const string &filetype_arg,
   char countBuf[12];
   frame_num = size_t(count);  snprintf(countBuf,12,"%04d",count++);
   const string filename =
-    string(basename_arg)
+    string(basename)
     + string(countBuf)
     + string(".")
-    + filetype_arg;
-  SbName filetype(filetype_arg.c_str());
-  ok = renderer->writeToFile(filename.c_str(), filetype);
+    + filetype;
+  SbName filetype_sb(filetype.c_str());
+  ok = renderer->writeToFile(filename.c_str(), filetype_sb);
   if (!ok) {cerr << "Failed to render" << endl; }
   delete (renderer);
   return (ok);
 }
 
 
-/// \brief Handle the --list command line option
+/// \brief Handle the --list command line option.  Spits out the list
+/// of filetypes that work for rendering to files
 void ListWriteFileTypes() {
   if (!SoDB::isInitialized()) SoDB::init();
 
-  // FIX: make sure there are no leaks!
   SbViewportRegion *viewport= new SbViewportRegion();
   SoOffscreenRenderer *r = new SoOffscreenRenderer(*viewport);
   const int numFileTypes = r->getNumWriteFiletypes();
@@ -290,32 +305,120 @@ bool CheckTypeValid(const string &type) {
 
 /// \brief Load dragger/way points from a disk file
 /// \param filename Ascii file to load from
-/// \param root Where in the scene graph location to add the draggers
+/// \param root Where in the scene graph location to add the draggers  (should be draggerSwitch
 /// \param draggerVec A vector in which to keep the way points handy
 /// \return \a false if trouble loading the draggers/waypoints
 bool
-LoadSpotLightDraggers (const string filename, SoSeparator *root, vector<SoSpotLightDragger *> &draggerVec) {
+LoadSpotLightDraggersBroken (const string filename, SoSeparator *root, vector<SoSpotLightDragger *> &draggerVec)
+{
   bool ok=true;
   assert(root);
   DebugPrintf(TRACE,("LoadSpotLightDraggers %s %d\n",filename.c_str(),int(draggerVec.size())));
 
-  // FIX: nuke the old waypoints
+  ifstream in(filename.c_str(),ios::in);
+  if (!in.is_open()){cerr<<"ERROR opening "<<filename<<endl; return(false);}
+  const size_t bufSize=256;
+  char buf[bufSize];
+#ifndef NDEBUG
+  memset(buf,0,bufSize);
+#endif
+  while (in.getline(buf,bufSize)) {
+    if ('#'==buf[0]) continue;
+    istringstream istr(buf);
+    float a1,a2,a3,r_angle,x,y,z,angle;
+    istr >> a1 >> a2 >> a3 >> r_angle >> x >> y >> z >> angle;
 
-  
-
+    SoSpotLightDragger *wpt = new SoSpotLightDragger;
+    draggerVec.push_back(wpt);
+    {
+      SbRotation rot(SbVec3f(a1,a2,a3),r_angle);
+      //SbRotation rot(SbVec3f(a3,a2,a1),r_angle); cout << "Swapped?\n";
+      
+      SbVec3f pos(x,y,z);
+      wpt->translation.setValue(pos);
+      wpt->rotation.setValue(rot);
+      wpt->angle=angle;
+    }
+    {
+      static int num=0;  char buf[128];
+      snprintf(buf, 128,"_%d",num++); // name can't start with a number
+      wpt->setName (buf);
+    }
+    SoSeparator *s = new SoSeparator;
+    s->addChild(wpt);
+    root->addChild(s); // draggerSwitch
+  }	
 
   return (ok);
-}
+}	
 
+
+//
+// USE THE QUATERNIAN
+//
+
+/// \brief Load dragger/way points from a disk file
+/// \param filename Ascii file to load from
+/// \param root Where in the scene graph location to add the draggers  (should be draggerSwitch
+/// \param draggerVec A vector in which to keep the way points handy
+/// \return \a false if trouble loading the draggers/waypoints
 bool
-SaveSpotLightDraggers (const string filename, vector<SoSpotLightDragger *> &draggerVec) {
+LoadSpotLightDraggers (const string filename, SoSeparator *root, vector<SoSpotLightDragger *> &draggerVec)
+{
+  bool ok=true;
+  assert(root);
+  DebugPrintf(TRACE,("LoadSpotLightDraggers %s %d\n",filename.c_str(),int(draggerVec.size())));
+
+  ifstream in(filename.c_str(),ios::in);
+  if (!in.is_open()){cerr<<"ERROR opening "<<filename<<endl; return(false);}
+  const size_t bufSize=256;
+  char buf[bufSize];
+#ifndef NDEBUG
+  memset(buf,0,bufSize);
+#endif
+  while (in.getline(buf,bufSize)) {
+    if ('#'==buf[0]) continue;
+    istringstream istr(buf);
+    float a1,a2,a3,r_angle,x,y,z,angle;
+    istr >> a1 >> a2 >> a3 >> r_angle >> x >> y >> z >> angle;
+
+    SoSpotLightDragger *wpt = new SoSpotLightDragger;
+    draggerVec.push_back(wpt);
+    {
+      SbRotation rot(SbVec3f(a1,a2,a3),r_angle);
+      //SbRotation rot(SbVec3f(a3,a2,a1),r_angle); cout << "Swapped?\n";
+      
+      SbVec3f pos(x,y,z);
+      wpt->translation.setValue(pos);
+      wpt->rotation.setValue(rot);
+      wpt->angle=angle;
+    }
+    {
+      static int num=0;  char buf[128];
+      snprintf(buf, 128,"_%d",num++); // name can't start with a number
+      wpt->setName (buf);
+    }
+    SoSeparator *s = new SoSeparator;
+    s->addChild(wpt);
+    root->addChild(s); // draggerSwitch
+  }	
+
+  return (ok);
+}	
+
+
+/// \brief Save an ascii file of all the draggers for editing and reloading
+/// \param filename file to write to
+/// \param draggerVec pointers to all the SoSpotLightDragger way points
+/// \return \a false if had trouble writing the file
+///
+/// \bug Does not seem to save the correct orientation or load ascii is buggy
+bool
+SaveSpotLightDraggersAscii (const string &filename, vector<SoSpotLightDragger *> &draggerVec) {
   bool ok=true;
   DebugPrintf(TRACE,("SaveSpotLightDraggers %s %d\n",filename.c_str(),int(draggerVec.size())));
 
-  if (0==draggerVec.size()) {
-    cerr << "WARNING: skipping save.  There are zero waypoints." << endl;
-    return(false);
-  }
+  if(0==draggerVec.size()){cerr<<"WARNING: skipping save.  There are 0 waypoints"<<endl;return(false);}
 
   ofstream o(filename.c_str(),ios::out);
   if (!o.is_open()) {cerr<<"ERROR: unable to open waypoint file: "<<filename<<endl; return (false);}
@@ -335,15 +438,56 @@ SaveSpotLightDraggers (const string filename, vector<SoSpotLightDragger *> &drag
     float x, y, z;
     xyz.getValue(x,y,z);
     const float angle = d->angle.getValue(); // FIX: do not currently use angle for anything
-    o << a1 << " "<< a2 << " "<< a3 
-      << " " << r_angle
-      << " " << x << " " << y << " " << z
-      << " " << angle << endl;
-
+    o << a1 << "\t"<< a2 << "\t"<< a3 
+      << "\t" << r_angle
+      << "\t" << x << "\t" << y << "\t" << z
+      << "\t" << angle << endl;
   }
 
   return (ok);
 } 
+
+/// \brief Save an ascii file of all the draggers for editing and reloading
+/// \param filename file to write to
+/// \param draggerVec pointers to all the SoSpotLightDragger way points
+/// \return \a false if had trouble writing the file
+bool
+SaveSpotLightDraggersIV(const string &filename, SoSwitch *sw) {
+  assert(sw);
+  if (0==sw->getNumChildren()) {cerr << "WARNING: No waypoints to write"<<endl;return(false);}
+  SoOutput o;
+  if (!o.openFile(filename.c_str())) {
+    cerr<<"ERROR: unable to open file to dump iv to: " << filename << endl;
+    return(false);
+  }
+
+  SoWriteAction wa(&o);
+  for (int i=0;i< sw->getNumChildren();i++) {
+    DebugPrintf(BOMBASTIC,("Writing switch child # %d\n",i));
+    wa.apply(sw->getChild(i));
+  }
+  
+
+  return (true);
+}
+
+/// \brief Write a scene graph to disk as ascii
+/// \param filename File to write ascii IV file to
+/// \param root Starting point of the scene graph to same
+/// \return \a false if we were not able to open the file.
+bool
+WriteSceneGraph (const string &filename, SoNode *root) {
+  SoOutput o;
+  if (!o.openFile(filename.c_str())) {
+    cerr<<"ERROR: unable to open file to dump iv to: " << filename << endl;
+    return(false);
+  }
+
+  SoWriteAction wa(&o);
+  wa.apply(root); // Error checking?
+  return (true);
+}
+
 
 /***************************************************************************
  * KEYBOARD STUFF
@@ -353,13 +497,14 @@ SaveSpotLightDraggers (const string filename, vector<SoSpotLightDragger *> &drag
 void PrintKeyboardShorts() {
   cout << endl
        << "Keyboard shortcuts:" << endl << endl
-       << "\t a - Toggle animation -  Start/Stop" << endl
-       << "\t d - Dump the current view to an image" << endl
-       << "\t h - Print out this help list" << endl
-       << "\t i - Insert a waypoint at the current camera location" << endl
-       << "\t r - Render frames as they animate" << endl
-       << "\t s - Show/Hide way point markers" << endl
-       << "\t w - Write the current set of waypoints to a file" << endl
+       << "\t a - Toggle (A)nimation -  Start/Stop" << endl
+       << "\t d - (D)ump the current view to an image" << endl
+       << "\t f - Write scene graph to a (F)File" << endl
+       << "\t h - Print out this (H)elp list" << endl
+       << "\t i - (I)nsert a waypoint at the current camera location" << endl
+       << "\t r - (R)Render frames as they animate" << endl
+       << "\t s - (S)how/Hide way point markers" << endl
+       << "\t w - Write the current set of (W)aypoints to a file" << endl
        << endl
        << "\t - - Decrease the debug level" << endl
        << "\t + - Incease  the debug level" << endl
@@ -372,12 +517,16 @@ void PrintKeyboardShorts() {
 
 /// \brief Gets called whenever the user presses a key in interactive mode
 /// \param data This will be the SceneInfo structure.
+/// \param cb SoKeyboardEvent that records the keypress or release
+///
+/// Make sure that this stays in sync with PrintKeyboardShorts()
 void
 keyPressCallback(void *data, SoEventCallback *cb) {
   assert(data); assert(cb);
 
   SceneInfo *si = (SceneInfo *)data;
-  assert(si->getMagicNumber()==SceneInfo::nominalMagicNumber());
+  //assert(si->getMagicNumber()==SceneInfo::nominalMagicNumber());
+  assert (si->magicOk());
 
   assert (si);
   assert (si->camera);
@@ -403,46 +552,32 @@ keyPressCallback(void *data, SoEventCallback *cb) {
   //
   if (SO_KEY_PRESS_EVENT(keyEvent, D)) {
     DebugPrintf(TRACE,("Keyhit: D - render  %d %d\n",si->a->width_arg,si->a->height_arg));
-    cerr << "d" << endl;
-
     size_t frame_num;
     if (!RenderFrameToDisk (string(si->a->basename_arg),string(si->a->type_arg),
-			si->a->width_arg, si->a->height_arg,
-			si->root, frame_num)
-	) {
+			    si->a->width_arg, si->a->height_arg,
+			    si->root, frame_num) )
       cerr << "ERROR: unable to write you artistic work.  You have been sensored.  Not my fault." << endl;
-    }
+
     DebugPrintf(TRACE+1,("Finished writing frame number %04d\n",int(frame_num)));
-
-#if 0
-    SbViewportRegion viewport(si->a->width_arg,si->a->height_arg);
-    SoOffscreenRenderer *renderer = new SoOffscreenRenderer(viewport);
-
-    SbVec2s maxRes = renderer->getMaximumResolution();
-    DebugPrintf(VERBOSE,("maxRes: %d %d\n",maxRes[0], maxRes[1]));
-    
-    SbBool ok = renderer->render(si->root);
-    if (!ok) {  cerr << "Unable to render!" << endl;  return;   }
-    {
-      static int count=0;
-      char countBuf[12];
-      snprintf(countBuf,12,"%04d",count++);
-      const string filename =
-	string(si->a->basename_arg)
-	+ string(countBuf)
-	+ string(".")
-	+ string(si->a->type_arg);
-      SbName filetype(si->a->type_arg);
-      ok = renderer->writeToFile(filename.c_str(), filetype);
-      if (!ok) {cerr << "Failed to render" << endl; }
-    }
-    delete (renderer);
-#endif
     return;
   } // KEY_PRESS D - dump screen
 
+  //
+  // H - Print Help to stdout
+  //
   if (SO_KEY_PRESS_EVENT(keyEvent, H)) { PrintKeyboardShorts(); return; }
 
+  //
+  // F - Write scene graph to a file
+  //
+  if (SO_KEY_PRESS_EVENT(keyEvent, F)) {
+    const string filename = "tmp.iv";
+    DebugPrintf(TRACE,("Keyhit: F - write scenegraph to a (F)ile: %s\n",filename.c_str()));
+    if (!WriteSceneGraph(filename, si->root)) {
+      cerr << "ERROR: Unable to write scene graph to file: " << filename << endl;
+    }
+    return;
+  }
 
   //
   // I - insert a waypoint marker
@@ -498,7 +633,6 @@ keyPressCallback(void *data, SoEventCallback *cb) {
   // S - show/hide waypoint markers
   //
   if (SO_KEY_PRESS_EVENT(keyEvent, S)) {
-    //cout << "(H)ide/show way points\n";
     if (SO_SWITCH_ALL == si->draggerSwitch->whichChild.getValue())
       si->draggerSwitch->whichChild = SO_SWITCH_NONE;
     else si->draggerSwitch->whichChild = SO_SWITCH_ALL;
@@ -509,14 +643,23 @@ keyPressCallback(void *data, SoEventCallback *cb) {
   // W - write waypoints to a file
   //
   if (SO_KEY_PRESS_EVENT(keyEvent, W)) {
-    const string filename("tmp.wpt");
-    DebugPrintf(TRACE,("Keyhit: W - write waypoints: %s\n",filename.c_str()));
-    if (!SaveSpotLightDraggers(filename, si->draggerVec))
-      cerr << "WARNING: waypoint save to " << filename << "failed." << endl;
+    const string filenameAscii("tmp.wpt.ascii");
+    const string filenameIV("tmp.wpt.iv");
+
+    DebugPrintf(TRACE,("Keyhit: W - write waypoints: %s %s\n",filenameAscii.c_str(),filenameIV.c_str()));
+    if (!SaveSpotLightDraggersIV(filenameIV, si->draggerSwitch))
+      cerr << "WARNING: waypoint save to " << filenameIV << "failed." << endl;
+
+     if (!SaveSpotLightDraggersAscii(filenameAscii, si->draggerVec))
+       cerr << "WARNING: waypoint save to " << filenameAscii << "failed." << endl;
     return;
   } // KEY_PRESS W - write waypoints
 
+
+  //
   // + - Increase the debugging level ... equal is that key with the '+' on it too.
+  //
+  // FIX: Describe which debug level we are at
   if (SO_KEY_PRESS_EVENT(keyEvent, EQUAL)) {
     debug_level++;
     DebugPrintf(TRACE,("Keyhit: + - Increase debug_level to %d\n",debug_level));
@@ -551,16 +694,9 @@ void timerSensorCallback(void *data, SoSensor *sensor) {
     initialized=false;
     return;  // not turned on right now
   }
-  if (si->draggerVec.size()<2) {
-    cout << "nothing to animate!" << endl;
-    return;
-  }
+  if (si->draggerVec.size()<2) { cout << "nothing to animate!" << endl;   return;  }
 
   // Now lets animate things!
-  if (0) {
-    static size_t tick=0;
-    cout << endl << "tick: " << tick++ << endl;
-  }
 
   static float percent=10.;
   static size_t cur=0;
@@ -579,7 +715,7 @@ void timerSensorCallback(void *data, SoSensor *sensor) {
     cout << "Switching to mark #" << cur << endl;
     //if (si->draggerVec.size()-1<=cur) {
     if (si->draggerVec.size()==cur) {
-      cout << "Looping" << endl;
+      //cout << "Looping" << endl;
       cur=0;
     }
   }
@@ -592,33 +728,24 @@ void timerSensorCallback(void *data, SoSensor *sensor) {
   SoSpotLightDragger *d2;
   if (cur==si->draggerVec.size()-1) {
     d2 = si->draggerVec[0];
-    cout << "Using " << cur << "-0" << endl;
+    //cout << "Using " << cur << "-0" << endl;
   } else d2 = si->draggerVec[cur+1];
 
   SbVec3f pos1 = d1->translation.getValue();
   SbVec3f pos2 = d2->translation.getValue();
-  //print (pos1);
-  //print (pos2);
 
   vector<float> v1 = ToVector(pos1);
   vector<float> v2 = ToVector(pos2);
-  //print (v1);
-  //print (v2);
+
   
   vector<float> v3 = InterpolatePos (v1,v2,percent);
-  //print (v3);
   SbVec3f pos3 = ToSbVec3f (v3);
-  //print (pos3);
 
   si->camera->position = pos3;
 
   SbRotation rot1 = d1->rotation.getValue();
   SbRotation rot2 = d2->rotation.getValue();
-#if 0
-  SbRotation newRot = InterpolateRotations(rot1,rot2,percent);
-#else
   SbRotation newRot = SbRotation::slerp (rot1, rot2, percent);
-#endif
 
   si->camera->orientation = newRot;
 
@@ -676,11 +803,6 @@ int main(int argc, char *argv[])
 
   SceneInfo *si = new SceneInfo;
   si->a=&a;
-//   si->root=root;
-//   //si->root=myViewer->getSceneManager()->getSceneGraph();
-
-//   //si->camera = myViewer->getCamera();
-
 
   si->root = new SoSeparator;
   si->root->ref();
@@ -712,14 +834,6 @@ int main(int argc, char *argv[])
   }
 
   
-//   SceneInfo *si = new SceneInfo;
-//   si->root=root;
-//   //si->root=myViewer->getSceneManager()->getSceneGraph();
-//   si->a=&a;
-//   //si->camera = myViewer->getCamera();
-
-
-
   myViewer->setSceneGraph( si->root );
   myViewer->show();
 
@@ -741,12 +855,16 @@ int main(int argc, char *argv[])
     si->draggerSwitch=s;
   }
 
+#if 0
   if (a.waypoints_given) {
     // FIX: make load go under an soswitch?
     if (!LoadSpotLightDraggers(string(a.waypoints_arg), (SoSeparator *)si->draggerSwitch, si->draggerVec)) {
       cerr << "WARNING: failed to load waypoints.  Continuing anyways." << endl;
     }
   }
+#else
+  cerr <<"FIX: loading draggers is disabled" <<endl;
+#endif
 
   // Write out keyboard shortcuts
   if (0<debug_level) PrintKeyboardShorts();
