f78eab92e14c72f284f4b7f0c00dcd5a141b2f10
Kurt Schwehr <schwehr@gmail.com>
2004-10-26 19:01:43 +0000

    removed  vector and angle functions to VecAngle.[CH]

--- Eigs.C
+++ Eigs.C
@@ -41,7 +41,9 @@
 // STL Headers
 #include <vector>
 
+// Local headers
 #include "Eigs.H"
+#include "VecAngle.H"
 
 using namespace std;
 
@@ -54,51 +56,9 @@ using namespace std;
 /// Let the debugger find out which version is being used.
 static const UNUSED char* RCSid ="@(#) $Id$";
 
-
-/***************************************************************************
- * Vector/Angle helpers
- ***************************************************************************/
-
-float normRadAngle(float angleRad) {
-  while (angleRad<0.) angleRad += 2*M_PI;
-  while (angleRad>2*M_PI) angleRad -= 2*M_PI;
-  assert (0<= angleRad && angleRad <= 2*M_PI);
-  return (angleRad);
-}
-
-
-void rotateXY (const float x1, const float y1, const float angleRad, float &x2, float &y2) {
-  //assert (0<= angleRad && angleRad <= 2*M_PI);
-  const float radius = sqrt(x1*x1 + y1*y1);
-  const float oldAngle = atan2(y1,x1);
-  const float newAngle = normRadAngle(oldAngle + angleRad);
-  x2 = radius * cos(newAngle);
-  y2 = radius * sin(newAngle);
-}
-
-
 /***************************************************************************
  * EIGS
  ***************************************************************************/
-
-
-// convert xyz to theta (t) and phi(p) in radians
-// go polar
-void xyz2tpr (const double x, const double y, const double z, double &theta, double &phi, double &radius) {
-  const double pi=M_PI;
-  radius = sqrt(x*x+y*y+z*z);
-  theta = acos(z/radius);
-  if (0. == x) {
-    if (y<0)  phi = 3 * pi * 0.5;
-    else phi = pi * 0.5;
-    return;
-  }
-  phi = atan(y/x);
-  if (x<0) phi += pi;
-  if (phi<0) phi += 2*pi;
-}
-
-
 
 
 size_t GetIndex(const EigsEnum which, const gsl_vector *v) {
@@ -181,16 +141,6 @@ GetEigs(const gsl_matrix *eigenvec, const gsl_vector *eigenval, float newEigs[9]
   flip(newEigs[7],newEigs[8]);
 
   return(ok);
-}
-
-void ldi2xyz( const float len, const float dec, const float inc, vector<float> &xyz) {
-  assert(xyz.size()==3);
-  const float incRad = deg2rad(inc);
-  const float decRad = deg2rad(dec);
-  const float x = len * cos(incRad) * sin(decRad);
-  const float y = len * cos(incRad) * cos(decRad);
-  const float z = -sin(incRad)*len;
-  xyz[0] = x; xyz[1]=y; xyz[2]=z;
 }
 
 
@@ -291,9 +241,9 @@ bool S_Engine::getXYZ(const EigsEnum which, vector<float> &xyz) const {
 //####################################################################
 #ifdef REGRESSION_TEST
 
-static bool isEqual (const float a, const float b, const float del) {
-  return ( ( a<b+del && a > b-del) ? true : false );
-}
+//static bool isEqual (const float a, const float b, const float del) {
+//  return ( ( a<b+del && a > b-del) ? true : false );
+//}
 
 bool test1() {
   //int r; // result from gsl calls.  0 is ok, !=0 is trouble
@@ -501,38 +451,6 @@ bool test3() {
   return(ok);
 }
 
-//float normRadAngle(float angleRad)
-// void rotateXY (const float x1, const float y1, const float angleRad, float &x2, float &y2)
-
-bool test4() {
-  cout << "      test4" << endl;
-  bool ok=true;
-
-  if (!isEqual(normRadAngle(-M_PI),M_PI,0.0001)) {FAILED_HERE;ok=false;}
-  if (!isEqual(normRadAngle(3*M_PI),M_PI,0.0001)) {FAILED_HERE;ok=false;}
-
-  float x1,y1,x2,y2;
-  x1=1.0; y1=0.0; rotateXY(x1,y1,0.,x2,y2);
-  if (!isEqual(x1,x2,0.0001)){FAILED_HERE;ok=false;}
-
-  float angle = M_PI/2;
-
-  rotateXY(x1,y1,angle,x2,y2);
-  cout << "  " << x1 << " " << y1 << " " << angle << "    " << x2 << " " << y2 << endl;
-  if (!isEqual(x2,0.f,0.0001)){FAILED_HERE;ok=false;}
-  if (!isEqual(y2,1.f,0.0001)){FAILED_HERE;ok=false;}
-
-
-  angle = -M_PI/2;
-  rotateXY(x1,y1,angle,x2,y2);
-  cout << "  " << x1 << " " << y1 << " " << angle << "    " << x2 << " " << y2 << endl;
-  if (!isEqual(x2,0.f,0.0001)){FAILED_HERE;ok=false;}
-  if (!isEqual(y2,-1.f,0.0001)){FAILED_HERE;ok=false;}
-
-
-  return (ok);
-}
-
 int main (UNUSED int argc, char *argv[]) {
   // Put test code here
   bool ok=true;
@@ -540,7 +458,6 @@ int main (UNUSED int argc, char *argv[]) {
   if (!test1()) {FAILED_HERE;ok=false;}
   if (!test2()) {FAILED_HERE;ok=false;}
   if (!test3()) {FAILED_HERE;ok=false;}
-  if (!test4()) {FAILED_HERE;ok=false;}
 
   cout << "  " << argv[0] << " test:  " << (ok?"ok":"failed")<<endl;
   return (ok?EXIT_SUCCESS:EXIT_FAILURE);
