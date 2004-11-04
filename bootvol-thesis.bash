78eb05a2d4ce4489372e1b020d7ca6d28df1010f
Kurt Schwehr <schwehr@gmail.com>
2004-11-04 17:45:59 +0000

    debugging... only do group g2 from ol92 Added die() to fail if command bomb

--- bootvol-thesis.bash
+++ bootvol-thesis.bash
@@ -37,6 +37,12 @@
 # Local tuning
 ######################################################################
 
+die()
+{
+    declare -ir line=$1
+    echo "ERROR: Command failed at line $line"
+    exit $EXIT_FAILURE
+}
 
 export PATH=${PATH}:.:..
 declare -ir cells=100
@@ -156,8 +162,8 @@ DebugEcho $TERSE $LINENO "# Owens Lake #"
 DebugEcho $TERSE $LINENO "##############"
 
 
-declare -ar ol_groups=( g1-fluidized g2-undeformed g3-sheared g4-little-def g5-intermediate )
-#declare -ar ol_groups=( g2-undeformed )
+#declare -ar ol_groups=( g1-fluidized g2-undeformed g3-sheared g4-little-def g5-intermediate )
+declare -ar ol_groups=( g2-undeformed )
 
 
 if [ ! -e "rosenbaum-ams-stripped.dat" ]; then
@@ -174,41 +180,43 @@ if [ 1 == 1 ]; then
 	DebugEcho $TERSE $LINENO "Processing $group"
 	# Should normed error be devided by 3?
 	awk '{print $32/3,$33/3,$34/3,$35/3,$36/3,$37/3,$5,$2}' $group.dat > $group.s
-	if [ ! -e ${group}-boot.xyz.vmax ]; then 
+	if [ ! -e ${group}-boot-vmax.xyz ]; then 
 	    if [ ! -e ${group}.s ]; then
 		echo "ERROR: ${group}.s is missing.  Goodbye."
 		exit $EXIT_FAILURE
 	    fi
-	    s_bootstrap ${group}.s -f xyz  -n 3 --out ${group}.xyz. -p --draw ${draw} -v $debugLevel
-	    mv ${group}.xyz.1.vmax ${group}-boot.xyz.vmax
-	    mv ${group}.xyz.2.vint ${group}-boot.xyz.vint
-	    mv ${group}.xyz.3.vmin ${group}-boot.xyz.vmin
+	    s_bootstrap ${group}.s -f xyz  -n 3 --out ${group}.xyz. -p --draw ${draw} -v $debugLevel || die $LINENO
+	    mv ${group}.xyz.1.vmax ${group}-boot-vmax.xyz
+	    mv ${group}.xyz.2.vint ${group}-boot-vint.xyz
+	    mv ${group}.xyz.3.vmin ${group}-boot-vmin.xyz
 	else
 	    DebugEcho $TERSE $LINENO "Using existing bootstrapped xyz's"
 	fi
 
 	DebugEcho $TRACE $LINENO  "Densifying:  calling xyzdensity 4 times"
 
-	args=" -p 1  --bpv=16 -w ${cells} -t ${cells} -d ${cells} --verbosity=$debugLevel"
+	echo "Only doing 8 bpv"
+	#args=" -p 1  --bpv=16 -w ${cells} -t ${cells} -d ${cells} --verbosity=$debugLevel"
+	args=" -p 1  --bpv=8 -w ${cells} -t ${cells} -d ${cells} --verbosity=$debugLevel"
 	if [ ! -e ${group}-vmax.vol ]; then
-	    xyzdensity ${group}-boot.xyz.vmax --out=${group}-vmax.vol $args $boundaries
-	    xyzdensity ${group}-boot.xyz.vint --out=${group}-vint.vol $args $boundaries
-	    xyzdensity ${group}-boot.xyz.vmin --out=${group}-vmin.vol $args $boundaries
+	    xyzdensity ${group}-boot-vmax.xyz --out=${group}-vmax-8.vol $args $boundaries || die $LINENO
+	    xyzdensity ${group}-boot-vint.xyz --out=${group}-vint-8.vol $args $boundaries || die $LINENO
+	    xyzdensity ${group}-boot-vmin.xyz --out=${group}-vmin-8.vol $args $boundaries || die $LINENO
 	    xyzdensity --out=${group}-all.vol -p 1 -b 8 -w ${cells} -t ${cells} -d ${cells} $boundaries \
-		${group}-boot.xyz.vmax \
-		${group}-boot.xyz.vint \
-		${group}-boot.xyz.vmin 
-	    volhdr_edit ${group}-all.vol --out=tmp.vol $scale && /bin/mv tmp.vol ${group}-all.vol
-	    volhdr_edit ${group}-vmax.vol --out=tmp.vol $scale && /bin/mv tmp.vol ${group}-vmax.vol
-	    volhdr_edit ${group}-vint.vol --out=tmp.vol $scale && /bin/mv tmp.vol ${group}-vint.vol
-	    volhdr_edit ${group}-vmin.vol --out=tmp.vol $scale && /bin/mv tmp.vol ${group}-vmin.vol
+		${group}-boot-vmax.xyz \
+		${group}-boot-vint.xyz \
+		${group}-boot-vmin.xyz  || die $LINENO
+	    volhdr_edit ${group}-all.vol --out=tmp.vol $scale && /bin/mv tmp.vol ${group}-all.vol || die $LINENO
+	    volhdr_edit ${group}-vmax-8.vol --out=tmp.vol $scale && /bin/mv tmp.vol ${group}-vmax-8.vol || die $LINENO
+	    volhdr_edit ${group}-vint-8.vol --out=tmp.vol $scale && /bin/mv tmp.vol ${group}-vint-8.vol || die $LINENO
+	    volhdr_edit ${group}-vmin-8.vol --out=tmp.vol $scale && /bin/mv tmp.vol ${group}-vmin-8.vol || die $LINENO
 	    #
             # Make each component viewable... scale them to min max
 	    #
 	    vol2vol_args="  -p 0 --bpv=8 -j 0.5 -k 0.5 -l 0.5 -v $debugLevel"
-	    vol2vol -o ${group}-vmax-8.vol ${group}-vmax.vol $vol2vol_args
-	    vol2vol -o ${group}-vint-8.vol ${group}-vint.vol $vol2vol_args
-	    vol2vol -o ${group}-vmin-8.vol ${group}-vmin.vol $vol2vol_args
+	    #vol2vol -o ${group}-vmax-8.vol ${group}-vmax.vol $vol2vol_args || die $LINENO
+	    #vol2vol -o ${group}-vint-8.vol ${group}-vint.vol $vol2vol_args || die $LINENO
+	    #vol2vol -o ${group}-vmin-8.vol ${group}-vmin.vol $vol2vol_args || die $LINENO
 	else
 	    DebugEcho $TERSE $LINENO "Using existing ${group}-vmax.vol"
 	fi
@@ -217,32 +225,30 @@ if [ 1 == 1 ]; then
 	    if [ ! -e rgba.cpt ]; then
 		DebugEcho $TRACE $LINENO "ERROR:  missing rgba.cpt"
 	    fi
-	    volmakecmap --cpt=rgba.cpt -o current.cmap --zero=0
+	    volmakecmap --cpt=rgba.cpt -o current.cmap --zero=0 || die $LINENO
 	fi
 
 	# --box=2.0
-	vol_iv -c ALPHA_BLENDING --numslicescontrol=ALL -p NONE -C current.cmap -o ${group}-all.iv ${group}-all.vol
-
+	vol_iv -c ALPHA_BLENDING --numslicescontrol=ALL -p NONE -C current.cmap -o ${group}-all.iv ${group}-all.vol \
+	|| die $LINENO
 
 	ivargs=" -c ALPHA_BLENDING --numslicescontrol=ALL -p NONE -C current.cmap -v $debugLevel"
-	vol_iv -o ${group}-vmax-8.iv ${group}-vmax-8.vol $ivargs
-	vol_iv -o ${group}-vint-8.iv ${group}-vint-8.vol $ivargs
-	vol_iv -o ${group}-vmin-8.iv ${group}-vmin-8.vol $ivargs
+	vol_iv -o ${group}-vmax-8.iv ${group}-vmax-8.vol $ivargs || die $LINENO
+	vol_iv -o ${group}-vint-8.iv ${group}-vint-8.vol $ivargs || die $LINENO
+	vol_iv -o ${group}-vmin-8.iv ${group}-vmin-8.vol $ivargs || die $LINENO
 
 	#
 	# Plot all the points as boxes
 	#
 	s_eigs < $group.s > $group.eigs
 	eigs2xyz.py $group.eigs > $group.xyz
-	awk '{print $1,$2,$3}' $group.xyz > $group.xyz.vmin
-	awk '{print $4,$5,$6}' $group.xyz > $group.xyz.vint
-	awk '{print $7,$8,$9}' $group.xyz > $group.xyz.vmax
+	awk '{print $1,$2,$3}' $group.xyz > $group.xyz.vmin || die $LINENO
+	awk '{print $4,$5,$6}' $group.xyz > $group.xyz.vint || die $LINENO
+	awk '{print $7,$8,$9}' $group.xyz > $group.xyz.vmax || die $LINENO
 
-	xyz_iv $box -p --color="1 0 0" --out=$group.xyz.vmin.iv -v 3 $group.xyz.vmin
-	xyz_iv $box -p --color="1 1 0" --out=$group.xyz.vint.iv -v 3 $group.xyz.vint
-	xyz_iv $box -p --color="0 0 1" --out=$group.xyz.vmax.iv -v 3 $group.xyz.vmax
-
-
+	xyz_iv $box -p --color="1 0 0" --out=$group.xyz.vmin.iv -v 3 $group.xyz.vmin || die $LINENO
+	xyz_iv $box -p --color="1 1 0" --out=$group.xyz.vint.iv -v 3 $group.xyz.vint || die $LINENO
+	xyz_iv $box -p --color="0 0 1" --out=$group.xyz.vmax.iv -v 3 $group.xyz.vmax || die $LINENO
     done
 fi
 
