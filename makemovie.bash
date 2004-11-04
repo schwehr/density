0888558ced72e09ff2b50cdc8dc66804e4a057ec
Kurt Schwehr <schwehr@gmail.com>
2004-11-04 21:10:10 +0000

    added die to catch problems faster now works great!

--- makemovie.bash
+++ makemovie.bash
@@ -10,12 +10,12 @@
 #     modify it under the terms of the GNU Lesser General Public
 #     License as published by the Free Software Foundation; either
 #     version 2.1 of the License, or (at your option) any later version.
-
+#
 #     This library is distributed in the hope that it will be useful,
 #     but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 #     Lesser General Public License for more details.
-
+#
 #     You should have received a copy of the GNU Lesser General Public
 #     License along with this library; if not, write to the Free Software
 #     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -69,6 +69,7 @@ declare -r render_args="axes.iv     -p 0.05  -w sample.wpt -L -W $size -H $size 
 
 
 declare -ar ol_groups=( g1-fluidized g2-undeformed g3-sheared g4-little-def g5-intermediate )
+#declare -ar ol_groups=( g2-undeformed )
 
 echo ${ol_groups[@]}
 
@@ -77,23 +78,23 @@ if [ 1 == 1 ]; then
 	DebugEcho $TERSE $LINENO "Rendering group $group"
 
 	FailIfNotThere $LINENO ${group}-all.iv
-	render -b ${group}-all- ${group}-all.iv $render_args
+	render -b ${group}-all- ${group}-all.iv $render_args || die $LINENO
 
 	FailIfNotThere $LINENO ${group}-vmax-8.iv
-        render -b ${group}-vmax- ${group}-vmax-8.iv $render_args
+        render -b ${group}-vmax- ${group}-vmax-8.iv $render_args $group-vmax.xyz.iv || die $LINENO
 
 	FailIfNotThere $LINENO ${group}-vint-8.iv
-        render -b ${group}-vint- ${group}-vint-8.iv $render_args
+        render -b ${group}-vint- ${group}-vint-8.iv $render_args $group-vint.xyz.iv || die $LINENO
 
 	FailIfNotThere $LINENO ${group}-vmin-8.iv
-        render -b ${group}-vmin- ${group}-vmin-8.iv $render_args
+        render -b ${group}-vmin- ${group}-vmin-8.iv $render_args $group-vmin.xyz.iv || die $LINENO
     done
 fi
 
-if [ 0 == 1 ]; then 
+if [ 1 == 1 ]; then 
     DebugEcho $TERSE $LINENO "Brightening all images"
-    for file in g*.png; do
-	convert -modulate 210 $file tmp-$file
+    for file in g[1-5]-*.png; do
+	convert -modulate 210 $file tmp-$file || die $LINENO
 	/bin/mv -f tmp-$file $file
     done
 fi
@@ -102,7 +103,7 @@ fi
 # Group all rendered frames for one step into one big movie frame
 # 
 
-if [ 0 == 1 ]; then 
+if [ 1 == 1 ]; then 
     for group in "${ol_groups[@]}"; do
 	declare -i fileNum=0
 	DebugEcho $TERSE $LINENO "Assemble frames for group: $group"
@@ -131,12 +132,12 @@ if [ 1 == 1 ]; then
 	f=`printf "%04d" $fileNum`
 	DebugEcho $TERSE $LINENO "Assemble composites to a superframe for $f"
 	for group in "${ol_groups[@]}"; do
-	    pngtopnm ${group}-${f}.png > ${group}.pnm
+	    pngtopnm ${group}-${f}.png > ${group}.pnm  || die $LINENO
 	done
-	pnmcat -tb g1-fluidized.pnm g2-undeformed.pnm > t1
-	pnmcat -tb g3-sheared.pnm   g4-little-def.pnm > t2
-	pnmcat -tb t1 t2 > t3
-	pnmcat -tb t3 g5-intermediate.pnm | pnmtopng > ol-$f.png
+	pnmcat -tb g1-fluidized.pnm g2-undeformed.pnm > t1  || die $LINENO
+	pnmcat -tb g3-sheared.pnm   g4-little-def.pnm > t2  || die $LINENO
+	pnmcat -tb t1 t2 > t3 || die $LINENO
+	pnmcat -tb t3 g5-intermediate.pnm | pnmtopng > ol-$f.png || die $LINENO
 	rm -f t{1,2,3}  g1-fluidized.pnm g2-undeformed.pnm g3-sheared.pnm  g4-little-def.pnm g5-intermediate.pnm
 	fileNum=$[fileNum+1]
     done
