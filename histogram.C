ea372b33041c5439bdc93206fa23743e5409e6bd
Kurt Schwehr <schwehr@gmail.com>
2004-11-14 20:08:40 +0000

    Added command line parsing with gengetopt Previous versions of histogram were WRONG.  Unequal bin boundaries. now lets the user decide min/max in addition to num bins or will auto    select min/max

--- histogram.C
+++ histogram.C
@@ -25,23 +25,30 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+
+// local includes
+#include "histogram_cmd.h"
+
 using namespace std;
 
 int main (int argc, char *argv[]) {
-  if (3!=argc) {
-    cerr << "USAGE:" << endl << endl
-	 << "\t" << argv[0] << " filename numBins" << endl;
-    return (EXIT_FAILURE);
-  }
-  string filename(argv[1]);
-  const size_t numBins(atoi(argv[2]));
-  assert (100000>numBins);
+  gengetopt_args_info a;  // a == args
+  if (0!=cmdline_parser(argc,argv,&a)){cerr<<"MELT DOWN: should never get here"<<endl;return (EXIT_FAILURE);}
+
+  //string filename(argv[1]);
+  //const size_t numBins(atoi(argv[2]));
+  assert (100000>a.bins_arg);
+  assert (0<a.bins_arg);
 
   vector<float> data;
   {
-    ifstream in(filename.c_str(),ios::in);
-    if (!bool(in)) {cerr << "failed to open file: " << filename << endl; exit(EXIT_FAILURE); }
-    float tmp;  while(in >> tmp) {data.push_back(tmp);}
+    for (size_t i=0;i<a.inputs_num;i++) {
+      string filename(a.inputs[i]);
+      cout << "Reading file: " << filename << endl;
+      ifstream in(filename.c_str(),ios::in);
+      if (!bool(in)) {cerr << "failed to open file: " << filename << endl; exit(EXIT_FAILURE); }
+      float tmp;  while(in >> tmp) {data.push_back(tmp);}
+    }
   }
 
   cerr << "data size: " << data.size() << endl;
@@ -49,36 +56,60 @@ int main (int argc, char *argv[]) {
 
   sort(data.begin(),data.end());
 
-  const float binSize = (*(data.end()-1) - *data.begin()) / numBins;
+  const float min=(a.min_given?a.min_arg:*data.begin());
+  const float max=(a.max_given?a.max_arg:*(data.end()-1));
+  const float binSize=(max-min)/a.bins_arg;
 
-  cout << "# Kurt's histogram tool" << endl
-       << "# number of bins: " << numBins << endl
-       << "# bin size: " << binSize << endl
-       << "# " << endl
-       << "# gnuplot command:" << endl
-       << "# " << endl
-       << "#      plot '" << filename << "' width lines" << endl
-       << "# " << endl
-       << "# Note: this could be done with step or fstep too" << endl
-       << endl;
+  ofstream o(a.out_arg,ios::out);
+  if (!o.is_open()) {cerr<<"ERROR: unable to open waypoint file: "<<a.out_arg<<endl; return (false);}
 
+  o << "# Kurt's histogram tool" << endl
+    << "# number of bins: " << a.bins_arg << endl
+    << "# bin size: " << binSize << endl
+    << "# " << endl
+    << "# gnuplot command:" << endl
+    << "# " << endl
+    << "#      plot '" << a.out_arg << "' width lines" << endl
+    << "# " << endl
+    << "# Note: this could be done with step or fstep too" << endl
+    << endl;
+
+#if 0
   size_t bin=0;
   size_t count=0; // number of samples in the bin
   size_t binStart=0;
-
-  cout << data[0] << " " << 0 << endl;
+  o << min << " " << 0 << endl;
   for (size_t i=0;i<data.size();i++,count++) {
-    const size_t curBin = int((data[i]-data[0])/binSize);
+    if (data[i]<min) continue; // skip data outside the range
+    const size_t curBin = int((data[i]-min)/binSize);
     if (curBin!=bin) {
-      cout << data[binStart] << " " << count << endl;
-      cout << data[i] << " " << count << endl;
+      o << data[binStart] << " " << count << endl;
+      o << data[i] << " " << count << endl;
       bin=curBin;
       count=0;
       binStart=i;
     }
   }
-  cout << *(data.end()-1) << " " << 0 << endl;
-  cout << "# Max value: " << *(data.end()-1) << endl;
+  o << max << " " << 0 << endl;
+  o << "# Max value: " << *(data.end()-1) << endl;
+#endif
+
+  o << min << " " << 0 << endl;
+  // FIX: use STL and make this more efficient!
+  for (size_t bin=0;bin<size_t(a.bins_arg);bin++) {
+    size_t count=0;
+    const float binmin = min + binSize * bin;
+    const float binmax = min + binSize * (bin+1);
+    for (size_t i=0;i<data.size();i++) {
+      if (data[i]<=binmin) continue;
+      if (data[i]>binmax) break;
+      count++;
+    }
+    o << binmin << " " << count << "\n"
+      << binmax << " " << count << endl;
+  }
+  o << max << " " << 0 << "\n";
+
 
   return (EXIT_SUCCESS);
 }
