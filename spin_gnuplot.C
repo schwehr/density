97832e6a400d1ab852b83ca5ea77945c898eaafa
Kurt Schwehr <schwehr@gmail.com>
2004-12-12 21:58:39 +0000

    Not near working

---
+++ spin_gnuplot.C
@@ -0,0 +1,127 @@
+// $Revision$  $Author$  $Date$
+/*
+    Copyright (C) 2004  Kurt Schwehr
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+/// \file
+/// \brief Produce frames for a gnuplot movie
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// local includes
+#include "spin_gnuplot_cmd.h"
+
+/***************************************************************************
+ * LOCAL MACROS and DEFINES
+ ***************************************************************************/
+
+#include "debug.H" // provides FAILED_HERE, UNUSED, DebugPrintf
+int debug_level;
+
+/***************************************************************************
+ * LOCAL FUNCTIONS
+ ***************************************************************************/
+
+/// \brief Read the contents of a file into a string.
+/// \param filename File to read from disk
+/// \param result Returns \a true if ok, \a false if something bad
+/// \return The entire file turned into a string
+
+/// Hope the file is small!  What happens with a huge beast of a file?<br>
+/// FIX: return a reference?<br>
+/// FIX: the params are not getting handled by doxygen.  Why?<br>
+string
+GetStringFromFile(const char *filename, bool &result) {
+  assert(filename);
+  result=true;  // Innocent until proven guilty
+  ifstream in(filename, ios::in);
+  if (!in.is_open()) {
+    cerr << "ERROR: unable to open file "<<filename<<" in " << __FUNCTION__ << endl;
+    result=false;
+    return (string("ERROR: unable to read ") + string(filename));
+  }
+
+  string lines;
+  const size_t bufSize=1024;
+  char buf[bufSize];
+  while (in.getline(buf,bufSize)) {
+    lines += buf;
+  }
+
+  return (lines);
+}
+
+/***************************************************************************
+ * MAIN 
+ ***************************************************************************/
+int main (int argc, char *argv[]) {
+  gengetopt_args_info a;  // a == args
+  if (0!=cmdline_parser(argc,argv,&a)){cerr<<"MELT DOWN: should never get here"<<endl;return (EXIT_FAILURE);}
+
+  debug_level=a.verbosity_arg;
+
+  if (a.FileInput_given && a.StringInput_given) {
+    cerr << "ERROR can not specify both File and String input for the  "<< endl;
+    return (EXIT_FAILURE);
+  }
+
+  ofstream out(a.out_arg, ios::out);
+  if (!out.is_open()) {
+    cerr << "ERROR: Failed to open " << a.out_arg << endl;
+    return (EXIT_FAILURE);
+  }
+
+  string plotCmd;
+  if (2==a.dimensions_arg) {
+    plotCmd = string("plot ");
+  } else  if (3==a.dimensions_arg) {
+    plotCmd = string("splot ");
+  } else {
+    // bad kitty! bad!
+    cerr << "ERROR: dimensions must be either 2 or 3!" << endl;
+    return (EXIT_FAILURE);
+  }
+
+  if (a.FileInput_given) {
+    bool result;
+    plotCmd += GetStringFromFile(a.FileInput_arg,result);
+    if (!result) {
+      cerr << "ERROR: unable to read plot string from " << a.FileInput_arg << endl;
+      exit(EXIT_FAILURE);
+    }
+  } else if (a.StringInput_given) {
+    plotCmd += string(a.StringInput_arg);
+  } else {
+    cerr << "ERROR: listen bub...  You need to specify either SpringInput or FileInput\n" 
+	 << "  I have nothing to plot, so I give up." << endl;
+    exit(EXIT_FAILURE);
+  }
+
+  DebugPrintf(TRACE,("Plot command: %s\n",plotCmd.c_str()));
+
+
+
+
+  return(EXIT_SUCCESS);
+} // main()
+
